de292f302651dbbbce7a3fb55d7f3bbfda6049bb
chayma.saidi <chaymasaidi15@gmail.com>
2024-01-26 12:08:48 +0100

    version1

---
+++ src/test/java/com/Bcm/BcmBillcomCatalogManegementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Bcm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BcmBillcomCatalogManegementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

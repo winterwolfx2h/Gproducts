278f3de160bc781fa379bb8efc87babad578f950
bahaeddine.chouari <bahaeddine.chouari@billcom-consulting.com>
2024-04-25 09:58:44 +0100

    Latest patch notes

--- src/main/java/com/Bcm/Controller/ProductOfferingController/ProductOfferingController.java
+++ src/main/java/com/Bcm/Controller/ProductOfferingController/ProductOfferingController.java
@@ -25,7 +25,6 @@ import org.springframework.web.context.request.WebRequest;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-import java.util.Objects;
 
 @RestController
 @RequiredArgsConstructor

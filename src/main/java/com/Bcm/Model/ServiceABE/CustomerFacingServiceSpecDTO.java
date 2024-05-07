29027ef6e9dd632d3ff1a5b9f836ddc7f7fb1d09
bahaeddine.chouari <bahaeddine.chouari@billcom-consulting.com>
2024-05-07 11:31:30 +0100

    "LatestPush"

---
+++ src/main/java/com/Bcm/Model/ServiceABE/CustomerFacingServiceSpecDTO.java
@@ -0,0 +1,24 @@
+package com.Bcm.Model.ServiceABE;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class CustomerFacingServiceSpecDTO {
+    private int CFSS_code;
+    private String externalId;
+    private String numPlanCode;
+    private String serviceSpecType;
+    private String status;
+    private String description;
+    private List<ResourceFacingServiceSpecDTO> resourceFacingServiceSpec;  // Nested DTO for resource details
+}
+
+

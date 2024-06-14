70d81f340595c987d862759f00177607f9f7b99a
bahaeddine.chouari <bahaeddine.chouari@billcom-consulting.com>
2024-06-14 09:33:24 +0100

    Merge "ProductPriceGroup was added to the eligMethod's @RequestParams inside of the GenInfoController"

--- src/main/java/com/Bcm/Service/Srvc/ProductOfferingSrvc/GeneralInfoService.java
+++ src/main/java/com/Bcm/Service/Srvc/ProductOfferingSrvc/GeneralInfoService.java
@@ -3,8 +3,9 @@ package com.Bcm.Service.Srvc.ProductOfferingSrvc;
 import com.Bcm.Exception.ProductOfferingNotFoundException;
 import com.Bcm.Model.Product.GeneralInfoDTO;
 import com.Bcm.Model.ProductOfferingABE.ProductOffering;
-import java.util.List;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public interface GeneralInfoService {
@@ -14,9 +15,6 @@ public interface GeneralInfoService {
   List<GeneralInfoDTO> getAllGeneralInfoDTOs();
 
   ProductOffering getProductOfferingById(int Product_id) throws ProductOfferingNotFoundException;
-
-  ProductOffering updatePOPrg(GeneralInfoDTO generalInfoDTO, int Product_id, int productPriceGroupCode)
-      throws ProductOfferingNotFoundException;
 
   ProductOffering updatePOPrice(GeneralInfoDTO generalInfoDTO, int Product_id, int productPriceCode)
       throws ProductOfferingNotFoundException;
@@ -28,6 +26,11 @@ public interface GeneralInfoService {
       throws ProductOfferingNotFoundException;
 
   ProductOffering updatePOEligibility(
-      GeneralInfoDTO generalInfoDTO, int Product_id, int channelCode, int entityCode, int eligibility_id)
+      GeneralInfoDTO generalInfoDTO,
+      int Product_id,
+      int channelCode,
+      int entityCode,
+      int eligibility_id,
+      int productPriceGroupCode)
       throws ProductOfferingNotFoundException;
 }

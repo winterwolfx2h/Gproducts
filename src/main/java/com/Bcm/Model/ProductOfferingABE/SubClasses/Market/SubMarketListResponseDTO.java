efcfb101e91c5b6501f9d677910277e655cd5955
bahaeddine.chouari <bahaeddine.chouari@billcom-consulting.com>
2024-06-27 15:25:15 +0100

    Merge "SubMarkets list got tweaked"

---
+++ src/main/java/com/Bcm/Model/ProductOfferingABE/SubClasses/Market/SubMarketListResponseDTO.java
@@ -0,0 +1,17 @@
+package com.Bcm.Model.ProductOfferingABE.SubClasses.Market;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class SubMarketListResponseDTO {
+    private int po_SubMarketCode;
+    private String subMarketName;
+    private String subMarketDescription;
+    private Integer po_MarketCode;
+}
